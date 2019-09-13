d4fdb08f2da6a5b4792fd902b2be320648be3404
Cybertech <yasholma1@gmail.com>
2019-09-13 16:39:39 +0100

    Add CanBalance.js to this repository

---
+++ canBalance.js
@@ -0,0 +1,29 @@
+function canBalance(array) {
+    let arraySum = sum(array);
+    let halfSum = arraySum / 2;
+    let temp = [];
+    if (arraySum != 0 && arraySum % 2 === 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (sum(temp) < halfSum) {
+                temp.push(array.pop(i));
+            }
+        }
+        if (sum(array) === sum(temp)) {
+            return [array.length, temp.length];
+        } else {
+            return -1;
+        }
+    } 
+    return -1;
+}
+
+function sum (arr) {
+    let s = 0;
+    arr.forEach(i => s += i);
+    return s;
+}
+
+
+console.log(canBalance([1, 7, 5, 2, 3]));
+
+
